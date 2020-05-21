315230b91d11b16a22e584bdd56bcda578e13b6d
bycgxss <bycgxss@gmail.com>
2020-05-22 01:12:12 +0800

    创建 <Layout/>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      <p>Labels.vue</p>
-    </div>
-
-    <Nav/>
-  </div>
+  <Layout>
+    <p>Labels.vue</p>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -15,15 +11,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
 
-  .content {
-    overflow: auto;
-    flex-grow: 1;
-  }
 </style>
