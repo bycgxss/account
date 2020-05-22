6bf956e5aaf6ae4082060c43b2c2abbbe36f5ad3
bycgxss <bycgxss@gmail.com>
2020-05-22 21:09:35 +0800

    优化 Notes 组件，使用 v-model 代替 :value 和 @input 绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,9 +2,7 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" :value="value"
-             @input="onInput"
-             placeholder="在这里输入你的备注">
+      <input type="text" v-model="value" placeholder="在这里输入你的备注">
     </label>
   </div>
 </template>
@@ -16,11 +14,6 @@
   @Component
   export default class Notes extends Vue {
     value = '';
-
-    onInput(event: KeyboardEvent) {
-      const input = event.target as HTMLInputElement;
-      this.value = input.value;
-    }
   }
 </script>
 
