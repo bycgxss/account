82929306f3a53b64d7b9f6407f71195252d92274
bycgxss <bycgxss@gmail.com>
2020-05-24 01:13:23 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,10 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注：" placeholder="在这里输入你的备注" @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注：" placeholder="在这里输入你的备注" @update:value="onUpdateNotes"/>
+    </div>
+
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -11,7 +14,7 @@
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
   import recordListModel from '@/models/recordListModel';
@@ -21,9 +24,8 @@
   const tagList = tagListModel.fetch();
 
 
-
   @Component({
-    components: {Tags, Notes, Types, NumberPad}
+    components: {Tags, FormItem, Types, NumberPad}
   })
 
   export default class Money extends Vue {
@@ -58,5 +60,9 @@
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
+  }
+
+  .notes {
+    padding: 12px 0;
   }
 </style>
