43a54621fea93f0b90fd6e1d51a13e271f74c09a
bycgxss <bycgxss@gmail.com>
2020-05-25 00:55:27 +0800

    EditLabel.vue 使用 Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,13 +5,23 @@ import createId from '@/lib/createId';
 
 Vue.use(Vuex);
 
+type RootState = {
+  recordList: RecordItem[];
+  tagList: Tag[];
+  currentTag?: Tag;
+}
 const store = new Vuex.Store({
   state: {
-    recordList: [] as RecordItem[],
-    tagList: [] as Tag[]
-  },
+    recordList: [],
+    tagList: [],
+    currentTag: undefined,
+  } as RootState,
 
   mutations: {
+    setCurrentTag(state, id: string) {
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
+
     fetchRecords(state) {
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
@@ -28,20 +38,18 @@ const store = new Vuex.Store({
     },
 
     fetchTags(state) {
-      return state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
     },
 
     createTag(state, name: string) {
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
         window.alert('您输入的标签已存在，请勿重复添加');
-        return 'duplicated';
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
       window.alert('添加成功');
-      return 'success';
     },
 
     saveTags(state) {
