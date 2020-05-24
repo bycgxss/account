f6e2ab9c52c3cfcf937d55b85681db78922d38cc
bycgxss <bycgxss@gmail.com>
2020-05-24 11:18:16 +0800

    可以编辑和删除标签

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,17 +1,17 @@
 <template>
-  <svg class="icon">
+  <svg class="icon" @click="$emit('click', $event)">
     <use :xlink:href="'#' + name"/>
   </svg>
 </template>
 
 <script lang="ts">
-  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
-  try {importAll(require.context('../assets/icons', true, /\.svg$/))} catch (error) {console.log(error)}
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
   export default {
     props: ['name'],
     name: 'Icon'
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
