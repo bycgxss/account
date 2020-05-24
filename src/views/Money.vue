5dd768bd90ba616ebb3e13810201f7f8069a9501
bycgxss <bycgxss@gmail.com>
2020-05-24 17:22:17 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -43,14 +43,12 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record)
     }
 
     @Watch('recordList')
     onRecordChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
