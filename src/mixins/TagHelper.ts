7d029441daead798ed88844add796f663fc5e5ce
bycgxss <bycgxss@gmail.com>
2020-05-26 01:27:33 +0800

    修复一进页面就弹出4个提示的 bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import component from 'vue-class-component';
 
+const map: { [key: string]: string } = {
+  'tag name duplicated': '您输入的标签已存在，请勿重复添加'
+};
+
 @component
 export class TagHelper extends Vue {
   createTag() {
     const name = window.prompt('请输入您的标签名');
     if (!name) {return window.alert('标签不能为空，请重新添加！');}
     this.$store.commit('createTag', name);
+    if (this.$store.state.createTagError) {
+      window.alert(map[this.$store.state.createTagError.message] || '未知错误');
+    }
   }
 }
 
