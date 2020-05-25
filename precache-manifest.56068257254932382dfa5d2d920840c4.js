8412bd5c2a976d591cbc289746644f02c8cb0eba
bycgxss <bycgxss@gmail.com>
2020-05-26 03:16:38 +0800

    deploy

---
+++ precache-manifest.56068257254932382dfa5d2d920840c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd90e19c76812499a78",
+    "url": "/account/css/app.36b2df3a.css"
+  },
+  {
+    "revision": "156f39840153fa4c27fa032a358d5ef8",
+    "url": "/account/index.html"
+  },
+  {
+    "revision": "fcd90e19c76812499a78",
+    "url": "/account/js/app.e85e97ab.js"
+  },
+  {
+    "revision": "e516a4916505fbeaafe7",
+    "url": "/account/js/chunk-vendors.8a73c593.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account/robots.txt"
+  }
+]);
