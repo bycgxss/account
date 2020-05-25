d73c488b39722ca16e4d4406c7c9af3654255441
bycgxss <bycgxss@gmail.com>
2020-05-25 19:57:09 +0800

    完成列表的 css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,6 +2,30 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li class="record" v-for="item in group.items" :key="item.id">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>¥{{item.amount}}</span>
+
+          </li>
+        </ol>
+      </li>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li class="record" v-for="item in group.items" :key="item.id">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>¥{{item.amount}}</span>
+
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 
 </template>
@@ -17,6 +41,30 @@
     components: {Tabs}
   })
   export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = { title: string; items: RecordItem[] };
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || {title: date, items: []};
+        hashTable[date].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
     interval = 'day';
     recordTypeList = recordTypeList;
@@ -25,19 +73,44 @@
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .type-tabs-item {
-    background: white;
+  ::v-deep {
+    .type-tabs-item {
+      background: white;
 
-    &.selected {
-      background: #c4c4c4;
+      &.selected {
+        background: #c4c4c4;
 
-      &::after {
-        display: none;
+        &::after {
+          display: none;
+        }
       }
+    }
+
+    .interval-tabs-item {
+      height: 48px;
     }
   }
 
-  ::v-deep .interval-tabs-item {
-    height: 48px;
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
   }
+
+  .title {
+    @extend %item;
+  }
+
+  .record {
+    background: white;
+    @extend %item;
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+
 </style>
